503be02196dd8bc901acfc1c11daebb29f11c073
archy <archy@localhost>
2026-04-14 17:06:25 +1200

    First (test) Commit

--- src/comp603/COMP603.java
+++ src/comp603/COMP603.java
@@ -15,6 +15,7 @@ public class COMP603 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Test Commit - Archy");
     }
     
 }
